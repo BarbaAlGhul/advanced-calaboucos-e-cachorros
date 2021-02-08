1267bac323ca772565efde65702228bbee0fe036
Anderson Purić de Albuquerque <anderson.stan5@gmail.com>
2021-02-08 16:50:13 +0100

    add field of view

--- tile_types.py
+++ tile_types.py
@@ -13,16 +13,35 @@ graphic_dt = np.dtype(
 
 tile_dt = np.dtype(
     [
-        ("walkable", np.bool),
-        ("transparent", np.bool),
-        ("dark", graphic_dt)
+        ("walkable", np.bool8),
+        ("transparent", np.bool8),
+        ("dark", graphic_dt),
+        ("light", graphic_dt)
     ]
 )
 
 
-def new_tile(*, walkable:int, transparent: int, dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]]) -> np.ndarray:
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+def new_tile(
+    *, walkable:int, 
+    transparent: int,
+    dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]]
+    ) -> np.ndarray:
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
+
+# SHROUD is used for unexplored and unseen tiles
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
 
-floor = new_tile(walkable=True, transparent=True, dark=(ord("."), (255, 255, 255), (50, 50, 150)))
-wall = new_tile(walkable=False, transparent=False, dark=(ord("#"), (255, 255, 255), (0, 0, 100)))
+floor = new_tile(
+    walkable=True,
+    transparent=True,
+    dark=(ord("."), (100, 100, 100), (0, 0, 0)),
+    light=(ord("."), (200, 200, 200), (32, 32, 32))
+    )
+wall = new_tile(
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("#"), (100, 100, 100), (0, 0, 0)),
+    light=(ord("#"), (200, 200, 200), (0, 0, 0))
+    )
