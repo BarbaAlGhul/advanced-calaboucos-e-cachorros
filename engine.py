ad36757089274a62364c15a1a62bdd1e83a9eac9
Anderson Purić de Albuquerque <anderson.stan5@gmail.com>
2021-02-08 21:27:11 +0100

    add enemies and add placeholders for interactions

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-from typing import Set, Iterable, Any
+from typing import Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
@@ -11,12 +11,15 @@ from game_map import GameMap
 
 
 class Engine:
-    def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
-        self.entities = entities
+    def __init__(self, event_handler: EventHandler, game_map: GameMap, player: Entity):
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
         self.update_fov()
+
+    def handle_enemy_turns(self) -> None:
+        for entity in self.game_map.entities - {self.player}:
+            print(f'The {entity.name} wonders when it will get to take a real turn')
 
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
@@ -26,14 +29,11 @@ class Engine:
                 continue
             
             action.perform(self, self.player)
+            self.handle_enemy_turns()
             self.update_fov()
     
     def render(self, console: Console, context: Context) -> None:
-        self.game_map.render(console)
-        for entity in self.entities:
-            if self.game_map.visible[entity.x, entity.y]:
-                console.print(entity.x, entity.y, entity.char, fg=entity.color)
-        
+        self.game_map.render(console)        
         context.present(console)
         console.clear()
 
--- game_map.py
+++ game_map.py
@@ -1,12 +1,19 @@
+from __future__ import annotations
+from typing import Iterable, Optional, TYPE_CHECKING
+
 import numpy as np
 from tcod.console import Console
 
 import tile_types
 
+if TYPE_CHECKING:
+    from entity import Entity
+
 
 class GameMap:
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: int, height: int, entities: Iterable[Entity] = ()):
         self.width, self.height = width, height
+        self.entities = entities
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
         self.visible = np.full((width, height), fill_value=False, order="F")
         self.explored = np.full((width, height), fill_value=False, order="F")
@@ -20,3 +27,14 @@ class GameMap:
             choicelist=[self.tiles["light"], self.tiles["dark"]],
             default=tile_types.SHROUD
         )
+
+        for entity in self.entities:
+            if self.visible[entity.x, entity.y]:
+                console.print(x=entity.x, y=entity.y, string=entity.char, fg=entity.color)
+
+
+    def get_blocking_entity_at_location(self, location_x: int, location_y: int) -> Optional[Entity]:
+        for entity in self.entities:
+            if entity.blocks_movement and entity.x == location_x and entity.y == location_y:
+                return entity
+        return None
